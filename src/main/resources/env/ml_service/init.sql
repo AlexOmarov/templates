a5c49f77b714213104b7420e9db726c0ff4eb4cc
Alex Omarov <decentboat@gmail.com>
2021-05-17 17:05:49 +0700

    New schema for clickhouse

--- src/main/resources/env/ml_service/init.sql
+++ src/main/resources/env/ml_service/init.sql
@@ -3,11 +3,60 @@ GRANT ALL ON ml_service.* TO ml_service;
 
 CREATE TABLE if not exists ml_service.history
 (
-    service LowCardinality(String),
-    client LowCardinality(String),
-    startDate DateTime,
+    service UUID,
+    client UUID,
+    updateTime DateTime,
+    updateType LowCardinality(String),
     id UUID
 )
 ENGINE = MergeTree
 ORDER BY service
 SETTINGS index_granularity = 8192
+
+/* Model vectors */
+CREATE TABLE service_vector (
+    id UUID,
+    service UUID,
+    updateDate DateTime,
+    version UInt64,
+    clusters Map(String, UInt64)
+)
+ENGINE = MergeTree
+SETTINGS index_granularity = 8192
+
+CREATE TABLE client_vector (
+    id UUID,
+    client UUID,
+    updateDate DateTime,
+    version UInt64,
+    clusters Map(String, UInt64)
+)
+ENGINE = MergeTree
+SETTINGS index_granularity = 8192
+
+CREATE TABLE markov (
+    id UUID,
+    updateDate DateTime,
+    version UInt64,
+    clusterA String,
+    clusterB String,
+    value UInt64
+)
+ENGINE = MergeTree
+SETTINGS index_granularity = 8192
+
+/* Actual clients and services */
+CREATE TABLE client (
+    id UUID,
+    code String(512)
+)
+ENGINE = MergeTree
+SETTINGS index_granularity = 8192
+
+CREATE TABLE service (
+    id UUID,
+    code String(512)
+)
+ENGINE = MergeTree
+SETTINGS index_granularity = 8192
+
